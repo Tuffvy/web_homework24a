50d80d3a1b80cb5dc670c2592b2c1b2ddb435eff
Tuffv <Synchr1c@outlook.com>
2022-10-29 00:04:47 +0800

    web homework 4_a

--- src/main/java/com/example/web_homework24/CalController.java
+++ src/main/java/com/example/web_homework24/CalController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
